5aa6c4a6c33f5237144445642dd73975a2e818f3
sergiuToporjinschi <sergiutoporjinschi@users.noreply.github.com>
2020-02-26 00:14:37 +0200

    tests

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -5,7 +5,9 @@ var helper = require("./testHelper.js");
 require('should-sinon');
 
 describe("Functions", function () {
+
     var node = require('../backEndNode.js');
+
     afterEach(function () {
         helper.mockedNode.context.context = {};
     });
@@ -97,6 +99,7 @@ describe("Functions", function () {
 
     describe("Testing function recalculate", () => {
         var backend;
+
         before(() => {
             backend = new node({
                 'group': "test",
@@ -113,23 +116,25 @@ describe("Functions", function () {
             // },
             helper.setMockedDate('2019-05-15T17:01:58.135Z'); //Wednesday
         });
+
         it('Recalculate based on calendar', (done) => {
             var infoNode = _.extend(_.cloneDeep(helper.defaNewInfoNode), {
                 "currentTemp": 25
             });
 
             backend.recalculate(helper.defaLastInfoNode, infoNode);
-            infoNode.currentSchedule.day.should.be.equal("Wednesday");
-            infoNode.currentSchedule.temp.should.be.equal(22);
-            infoNode.currentSchedule.time.should.be.equal("16:40");
+            infoNode.currentSchedule.day.should.be.equal("Wednesday", "currentSchedule should be Wednesday 2019-05-15T17:01:58.135Z");
+            infoNode.currentSchedule.temp.should.be.equal(22, "currentSchedule temperature should be taken from calendar");
+            infoNode.currentSchedule.time.should.be.equal("16:40", "currentSchedule should be Wednesday 2019-05-15T17:01:58.135Z");
 
-            infoNode.nextSchedule.day.should.be.equal("Wednesday");
-            infoNode.nextSchedule.temp.should.be.equal(19);
-            infoNode.nextSchedule.time.should.be.equal("23:59");
+            infoNode.nextSchedule.day.should.be.equal("Wednesday", "nextSchedule should be Wednesday 2019-05-15T23:59:00.135Z");
+            infoNode.nextSchedule.temp.should.be.equal(19, "nextSchedule temperature should be taken from calendar from next step");
+            infoNode.nextSchedule.time.should.be.equal("23:59", "nextSchedule should be Wednesday 2019-05-15T23:59:00.135Z");
 
-            infoNode.targetValue.should.be.equal(infoNode.currentSchedule.temp);
+            infoNode.targetValue.should.be.equal(infoNode.currentSchedule.temp, "targetValue should be the same with the one from currentSchedule");
             done();
         });
+
         it('Recalculate based on user request', (done) => {
             var lastInfoNode = _.extend(_.cloneDeep(helper.defaLastInfoNode), {
                 "currentSchedule": backend.getScheduleTemp(helper.calendar),
@@ -147,28 +152,70 @@ describe("Functions", function () {
             });
 
             backend.recalculate(lastInfoNode, newInfoNode);
-            newInfoNode.targetValue.should.be.equal(newInfoNode.userTargetValue);
+            newInfoNode.targetValue.should.be.equal(newInfoNode.userTargetValue, "targetValue should be taken from userTargetValue when user changes the target temperature");
             done();
         });
-        it('User changes are locked, curentTemp', (done) => {
-            var lastInfoNode = _.extend(_.cloneDeep(helper.defaLastInfoNode), {
-                "currentSchedule": backend.getScheduleTemp(helper.calendar),
-                "nextSchedule": backend.getScheduleTemp(helper.calendar, 1),
-                "currentHeaterStatus": "off",
-                "currentTemp": 22,
-                "userTargetValue": 20
-            });
-            var newInfoNode = _.extend(_.cloneDeep(helper.defaNewInfoNode), {
-                "currentSchedule": backend.getScheduleTemp(helper.calendar),
-                "nextSchedule": backend.getScheduleTemp(helper.calendar, 1),
-                "currentTemp": 22,
-                "userTargetValue": 30,
-                "isUserCustom": true
+        describe("User changes", () => {
+            var lastInfoNode;
+            var newInfoNode;
+
+            before(() => {
+                lastInfoNode = _.extend(_.cloneDeep(helper.defaLastInfoNode), {
+                    "currentSchedule": backend.getScheduleTemp(helper.calendar),
+                    "nextSchedule": backend.getScheduleTemp(helper.calendar, 1),
+                    "currentHeaterStatus": "off",
+                    "currentTemp": 22,
+                    "userTargetValue": 20
+                });
+                newInfoNode = _.extend(_.cloneDeep(helper.defaNewInfoNode), {
+                    "currentSchedule": backend.getScheduleTemp(helper.calendar),
+                    "nextSchedule": backend.getScheduleTemp(helper.calendar, 1),
+                    "currentTemp": 22,
+                    "userTargetValue": 30,
+                    "isUserCustom": true
+                });
             });
 
-            backend.recalculate(lastInfoNode, newInfoNode);
-            newInfoNode.targetValue.should.be.equal(newInfoNode.userTargetValue);
-            done();
+            it('increase temperature by user', (done) => {
+                backend.recalculate(lastInfoNode, newInfoNode);
+                newInfoNode.targetValue.should.be.equal(newInfoNode.userTargetValue, "targetValue should be taken from userTargetValue");
+                newInfoNode.currentHeaterStatus.should.be.equal("on", "heater should be started if user increases it");
+                done();
+            });
+
+            it('decrese temperature by user', (done) => {
+                lastInfoNode = _.cloneDeep(newInfoNode);
+                newInfoNode = _.extend(_.cloneDeep(lastInfoNode), {
+                    "userTargetValue": 10
+                });
+                backend.recalculate(lastInfoNode, newInfoNode);
+                newInfoNode.targetValue.should.be.equal(newInfoNode.userTargetValue, "targetValue should be taken from userTargetValue");
+                newInfoNode.currentHeaterStatus.should.be.equal("off", "heater should be stopped if user decresses it");
+                done();
+            });
+
+            it('user is setting locked', (done) => {
+                lastInfoNode = _.cloneDeep(newInfoNode);
+                newInfoNode = _.extend(_.cloneDeep(lastInfoNode), {
+                    "isUserCustomLocked": true
+                });
+                backend.recalculate(lastInfoNode, newInfoNode);
+                newInfoNode.targetValue.should.be.equal(lastInfoNode.targetValue, "targetValue shuld not be changed");
+                newInfoNode.isUserCustomLocked.should.be.true("isUserCustomLocked should be true");
+                done();
+            });
+
+            it('calendar is changing', (done) => {
+                //
+                helper.setMockedDate('2019-05-15T13:01:58.135Z'); //Wednesday
+                lastInfoNode = _.cloneDeep(newInfoNode);
+                newInfoNode = _.extend(_.cloneDeep(lastInfoNode), {
+                    "currentTemp": 25
+                });
+                backend.recalculate(lastInfoNode, newInfoNode);
+                newInfoNode.targetValue.should.be.equal(newInfoNode.userTargetValue);
+                done();
+            });
         });
     })
 });
